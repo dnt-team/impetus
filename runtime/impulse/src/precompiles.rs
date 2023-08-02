6f5ad839206d4e1dbe7c5b186dd64afae50105d1
Trần Huy Duẫn <20841392+DuanTranHuy@users.noreply.github.com>
2023-08-02 21:26:46 +0700

    Add impulse (#22)
    Co-authored-by: Huy Duan Tran <duanthse03268@gmail.com>

---
+++ runtime/impulse/src/precompiles.rs
@@ -0,0 +1,96 @@
+use pallet_evm::{
+	IsPrecompileResult, Precompile, PrecompileHandle, PrecompileResult, PrecompileSet,
+};
+use sp_core::H160;
+use sp_std::marker::PhantomData;
+
+use pallet_evm_precompile_batch::BatchPrecompile;
+use pallet_evm_precompile_lucky_number::LuckyNumberPrecompile;
+use pallet_evm_precompile_modexp::Modexp;
+use pallet_evm_precompile_sha3fips::Sha3FIPS256;
+use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
+
+pub struct FrontierPrecompiles<R>(PhantomData<R>);
+
+impl<R> FrontierPrecompiles<R>
+where
+	R: pallet_evm::Config,
+{
+	pub fn new() -> Self {
+		Self(Default::default())
+	}
+	pub fn used_addresses() -> [H160; 9] {
+		[
+			hash(1),
+			hash(2),
+			hash(3),
+			hash(4),
+			hash(5),
+			hash(1024),
+			hash(1025),
+			hash(2052),
+			hash(2056),
+		]
+	}
+}
+impl<R> PrecompileSet for FrontierPrecompiles<R>
+where
+	R: pallet_evm::Config,
+	BatchPrecompile<R>: Precompile,
+	LuckyNumberPrecompile<R>: Precompile,
+{
+	fn execute(&self, handle: &mut impl PrecompileHandle) -> Option<PrecompileResult> {
+		match handle.code_address() {
+			// Ethereum precompiles :
+			a if a == hash(1) => {
+				log::info!("pre1 {:?}", a);
+				Some(ECRecover::execute(handle))
+			}
+			a if a == hash(2) => {
+				log::info!("pre2 {:?}", a);
+				Some(Sha256::execute(handle))
+			}
+			a if a == hash(3) => {
+				log::info!("pre3 {:?}", a);
+				Some(Ripemd160::execute(handle))
+			}
+			a if a == hash(4) => {
+				log::info!("pre4 {:?}", a);
+				Some(Identity::execute(handle))
+			}
+			a if a == hash(5) => {
+				log::info!("pre5 {:?}", a);
+				Some(Modexp::execute(handle))
+			}
+			// Non-Frontier specific nor Ethereum precompiles :
+			a if a == hash(1024) => {
+				log::info!("pre6 {:?}", a);
+				Some(Sha3FIPS256::execute(handle))
+			}
+			a if a == hash(1025) => {
+				log::info!("pre7 {:?}", a);
+				Some(ECRecoverPublicKey::execute(handle))
+			}
+			a if a == hash(2052) => {
+				log::info!("pre8 {:?}", a);
+				Some(LuckyNumberPrecompile::execute(handle))
+			}
+			a if a == hash(2056) => {
+				log::info!("pre9 {:?}", a);
+				Some(BatchPrecompile::execute(handle))
+			}
+			_ => None,
+		}
+	}
+
+	fn is_precompile(&self, address: H160, _gas: u64) -> IsPrecompileResult {
+		IsPrecompileResult::Answer {
+			is_precompile: Self::used_addresses().contains(&address),
+			extra_cost: 0,
+		}
+	}
+}
+
+fn hash(a: u64) -> H160 {
+	H160::from_low_u64_be(a)
+}
