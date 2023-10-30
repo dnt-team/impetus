b11d69a3f683c4368ba863199e80b2164ccc961c
Tran Huy Duan <20841392+DuanTranHuy@users.noreply.github.com>
2023-10-30 19:56:07 +0700

    node

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,27 +1,34 @@
-use std::{collections::BTreeMap, str::FromStr};
+use std::{ collections::BTreeMap, str::FromStr };
 
-use serde::{Deserialize, Serialize};
+use hex_literal::hex;
+use serde::{ Deserialize, Serialize };
 // Substrate
-use sc_chain_spec::{ChainType, Properties};
+use sc_chain_spec::{ ChainType, Properties };
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_consensus_grandpa::AuthorityId as GrandpaId;
 #[allow(unused_imports)]
 use sp_core::ecdsa;
-use sp_core::{sr25519, storage::Storage, Pair, Public, H160, U256};
-use sp_runtime::traits::{IdentifyAccount, Verify};
+use sp_core::{ sr25519, storage::Storage, Pair, Public, H160, U256 };
+use sp_runtime::traits::{ IdentifyAccount, Verify };
 use sp_state_machine::BasicExternalities;
 // Frontier
 use impetus_runtime::{
-	AccountId, Balance, EnableManualSeal, GenesisConfig, SS58Prefix, Signature, WASM_BINARY, ManagerCommitteeConfig
+	AccountId,
+	Balance,
+	EnableManualSeal,
+	RuntimeGenesisConfig,
+	SS58Prefix,
+	Signature,
+	WASM_BINARY,
+	ManagerCommitteeConfig,
 };
-use commons::pre_deploy_contracts::{ERC1820_REGISTRY, MULTICALL2_BYTECODE, MULTICALL3_BYTECODE};
-
+use commons::pre_deploy_contracts::{ ERC1820_REGISTRY, MULTICALL2_BYTECODE, MULTICALL3_BYTECODE };
 
 // The URL for the telemetry server.
 // const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
-pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
+pub type ChainSpec = sc_service::GenericChainSpec<RuntimeGenesisConfig>;
 
 /// Specialized `ChainSpec` for development.
 pub type DevChainSpec = sc_service::GenericChainSpec<DevGenesisExt>;
@@ -30,7 +37,7 @@ pub type DevChainSpec = sc_service::GenericChainSpec<DevGenesisExt>;
 #[derive(Serialize, Deserialize)]
 pub struct DevGenesisExt {
 	/// Genesis config.
-	genesis_config: GenesisConfig,
+	genesis_config: RuntimeGenesisConfig,
 	/// The flag that if enable manual-seal mode.
 	enable_manual_seal: Option<bool>,
 }
@@ -60,8 +67,7 @@ type AccountPublic = <Signature as Verify>::Signer;
 /// For use with `AccountId32`, `dead_code` if `AccountId20`.
 #[allow(dead_code)]
 pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId
-where
-	AccountPublic: From<<TPublic::Pair as Pair>::Public>,
+	where AccountPublic: From<<TPublic::Pair as Pair>::Public>
 {
 	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
 }
@@ -73,7 +79,7 @@ pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
 
 fn properties() -> Properties {
 	let mut properties = Properties::new();
-	properties.insert("tokenDecimals".into(), 18.into());
+	properties.insert("tokenDecimals".into(), (18).into());
 	properties.insert("ss58Format".into(), SS58Prefix::get().into());
 	properties
 }
@@ -100,15 +106,13 @@ pub fn development_config(enable_manual_seal: Option<bool>) -> DevChainSpec {
 						get_account_id_from_seed::<sr25519::Public>("Alice"),
 						get_account_id_from_seed::<sr25519::Public>("Bob"),
 						get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-						get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+						get_account_id_from_seed::<sr25519::Public>("Bob//stash")
 					],
 					// Initial PoA authorities
 					vec![authority_keys_from_seed("Alice")],
 					// Ethereum chain ID
 					322 as u64,
-					vec![
-						get_account_id_from_seed::<sr25519::Public>("Alice"),
-					],
+					vec![get_account_id_from_seed::<sr25519::Public>("Alice")]
 				),
 				enable_manual_seal,
 			}
@@ -124,7 +128,7 @@ pub fn development_config(enable_manual_seal: Option<bool>) -> DevChainSpec {
 		// Properties
 		Some(properties()),
 		// Extensions
-		None,
+		None
 	)
 }
 
@@ -156,16 +160,11 @@ pub fn local_testnet_config() -> ChainSpec {
 					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
 					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
 					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash")
 				],
-				vec![
-					authority_keys_from_seed("Alice"),
-					authority_keys_from_seed("Bob"),
-				],
+				vec![authority_keys_from_seed("Alice"), authority_keys_from_seed("Bob")],
 				322,
-				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-				],
+				vec![get_account_id_from_seed::<sr25519::Public>("Alice")]
 			)
 		},
 		// Bootnodes
@@ -179,8 +178,7 @@ pub fn local_testnet_config() -> ChainSpec {
 		// Properties
 		None,
 		// Extensions
-		None,
-		
+		None
 	)
 }
 
@@ -191,20 +189,25 @@ fn testnet_genesis(
 	endowed_accounts: Vec<AccountId>,
 	initial_authorities: Vec<(AuraId, GrandpaId)>,
 	chain_id: u64,
-	did_managers: Vec<AccountId>,
-) -> GenesisConfig {
+	did_managers: Vec<AccountId>
+) -> RuntimeGenesisConfig {
 	use impetus_runtime::{
-		AuraConfig, BalancesConfig, EVMChainIdConfig, EVMConfig, GrandpaConfig, SudoConfig,
-		SystemConfig, DidConfig
+		AuraConfig,
+		BalancesConfig,
+		EVMChainIdConfig,
+		EVMConfig,
+		GrandpaConfig,
+		SudoConfig,
+		SystemConfig,
+		DidConfig,
 	};
 
-	let num_endowed_accounts = endowed_accounts.len();
-
-	GenesisConfig {
+	RuntimeGenesisConfig {
 		// System
 		system: SystemConfig {
 			// Add Wasm runtime to storage.
 			code: wasm_binary.to_vec(),
+			..Default::default()
 		},
 		sudo: SudoConfig {
 			// Assign network admin rights.
@@ -223,17 +226,24 @@ fn testnet_genesis(
 
 		// Consensus
 		aura: AuraConfig {
-			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
+			authorities: initial_authorities
+				.iter()
+				.map(|x| x.0.clone())
+				.collect(),
 		},
 		grandpa: GrandpaConfig {
 			authorities: initial_authorities
 				.iter()
 				.map(|x| (x.1.clone(), 1))
 				.collect(),
+			..Default::default()
 		},
 
 		// EVM compatibility
-		evm_chain_id: EVMChainIdConfig { chain_id },
+		evm_chain_id: EVMChainIdConfig {
+			chain_id,
+			..Default::default()
+		},
 		evm: EVMConfig {
 			accounts: {
 				let mut map = BTreeMap::new();
@@ -243,74 +253,83 @@ fn testnet_genesis(
 					// SS58: 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
 					// hex: 0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d
 					// Using the full hex key, truncating to the first 20 bytes (the first 40 hex chars)
-					H160::from_str("d43593c715fdd31c61141abd04a99fd6822c8558")
-						.expect("internal H160 is valid; qed"),
+					H160::from_str("d43593c715fdd31c61141abd04a99fd6822c8558").expect(
+						"internal H160 is valid; qed"
+					),
 					fp_evm::GenesisAccount {
-						balance: U256::from_str("0xffffffffffffffffffffffffffffffff")
-							.expect("internal U256 is valid; qed"),
+						balance: U256::from_str("0xffffffffffffffffffffffffffffffff").expect(
+							"internal U256 is valid; qed"
+						),
 						code: Default::default(),
 						nonce: Default::default(),
 						storage: Default::default(),
-					},
+					}
 				);
 				map.insert(
 					// H160 address of CI test runner account
-					H160::from_str("6be02d1d3665660d22ff9624b7be0551ee1ac91b")
-						.expect("internal H160 is valid; qed"),
+					H160::from_str("6be02d1d3665660d22ff9624b7be0551ee1ac91b").expect(
+						"internal H160 is valid; qed"
+					),
 					fp_evm::GenesisAccount {
-						balance: U256::from_str("0xffffffffffffffffffffffffffffffff")
-							.expect("internal U256 is valid; qed"),
+						balance: U256::from_str("0xffffffffffffffffffffffffffffffff").expect(
+							"internal U256 is valid; qed"
+						),
 						code: Default::default(),
 						nonce: Default::default(),
 						storage: Default::default(),
-					},
+					}
 				);
 				map.insert(
 					// H160 address of multicall2
-					H160::from_str("5BA1e12693Dc8F9c48aAD8770482f4739bEeD696")
-						.expect("internal H160 is valid; qed"),
+					H160::from_str("5BA1e12693Dc8F9c48aAD8770482f4739bEeD696").expect(
+						"internal H160 is valid; qed"
+					),
 					fp_evm::GenesisAccount {
 						balance: U256::zero(),
 						code: hex::decode(MULTICALL2_BYTECODE.trim_end()).unwrap(),
 						nonce: Default::default(),
 						storage: Default::default(),
-					},
+					}
 				);
 				map.insert(
 					// H160 address of multicall3
-					H160::from_str("cA11bde05977b3631167028862bE2a173976CA11")
-						.expect("internal H160 is valid; qed"),
+					H160::from_str("cA11bde05977b3631167028862bE2a173976CA11").expect(
+						"internal H160 is valid; qed"
+					),
 					fp_evm::GenesisAccount {
 						balance: U256::zero(),
 						code: hex::decode(MULTICALL3_BYTECODE.trim_end()).unwrap(),
 						nonce: Default::default(),
 						storage: Default::default(),
-					},
+					}
 				);
 				map.insert(
 					// H160 address of 1820 registry
-					H160::from_str("1820a4B7618BdE71Dce8cdc73aAB6C95905faD24")
-						.expect("internal H160 is valid; qed"),
+					H160::from_str("1820a4B7618BdE71Dce8cdc73aAB6C95905faD24").expect(
+						"internal H160 is valid; qed"
+					),
 					fp_evm::GenesisAccount {
 						balance: U256::zero(),
 						code: hex::decode(ERC1820_REGISTRY.trim_end()).unwrap(),
 						nonce: Default::default(),
 						storage: Default::default(),
-					},
+					}
 				);
 				map.insert(
 					// H160 address for benchmark usage
-					H160::from_str("1000000000000000000000000000000000000001")
-						.expect("internal H160 is valid; qed"),
+					H160::from_str("1000000000000000000000000000000000000001").expect(
+						"internal H160 is valid; qed"
+					),
 					fp_evm::GenesisAccount {
 						nonce: U256::from(1),
 						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
 						storage: Default::default(),
 						code: vec![0x00],
-					},
+					}
 				);
 				map
 			},
+			..Default::default()
 		},
 		manager_committee: ManagerCommitteeConfig {
 			members: endowed_accounts
